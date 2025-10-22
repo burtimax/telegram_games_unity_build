3b97419337937359d3ffe74ba324eefc49cf1a07
Timofey Burkush <timofeyburkush@gmail.com>
2025-10-22 14:26:14 +0300

    -Обновил кеш

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'unity-game-cache-v1';
-const METADATA_CACHE = 'unity-game-metadata-v1';
+﻿const CACHE_NAME = 'unity-game-cache-v2';
+const METADATA_CACHE = 'unity-game-metadata-v2';
 const TTL_DAYS = 14;
 const TTL_MS = TTL_DAYS * 24 * 60 * 60 * 1000;
 
