96343c56e46b576b3fdb6fd5600c72886647157f
DinoCarbone <161222305+DinoCarbone@users.noreply.github.com>
2025-10-27 20:17:47 +0300

    refactor display draw prefub

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'unity-game-cache-v6';
-const METADATA_CACHE = 'unity-game-metadata-v6';
+﻿const CACHE_NAME = 'unity-game-cache-v7';
+const METADATA_CACHE = 'unity-game-metadata-v7';
 const TTL_DAYS = 14;
 const TTL_MS = TTL_DAYS * 24 * 60 * 60 * 1000;
 
