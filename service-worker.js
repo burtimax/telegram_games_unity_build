475546330c3d63fe12fb417f057ea02792758592
DinoCarbone <161222305+DinoCarbone@users.noreply.github.com>
2025-10-28 10:02:12 +0300

    fix bundle bug

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'unity-game-cache-v7';
-const METADATA_CACHE = 'unity-game-metadata-v7';
+﻿const CACHE_NAME = 'unity-game-cache-v8';
+const METADATA_CACHE = 'unity-game-metadata-v';
 const TTL_DAYS = 14;
 const TTL_MS = TTL_DAYS * 24 * 60 * 60 * 1000;
 
