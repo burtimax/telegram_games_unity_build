fa2946d2ab9e0f2190d6ff053840f05d41a49943
DinoCarbone <161222305+DinoCarbone@users.noreply.github.com>
2025-11-22 17:08:01 +0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'unity-game-cache-v18';
-const METADATA_CACHE = 'unity-game-metadata-v18';
+﻿const CACHE_NAME = 'unity-game-cache-v19';
+const METADATA_CACHE = 'unity-game-metadata-v19';
 const TTL_DAYS = 14;
 const TTL_MS = TTL_DAYS * 24 * 60 * 60 * 1000;
 
