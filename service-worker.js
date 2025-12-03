f0f56033d0ad0faf0809b859f62bd824e5b5f51e
Timofey Burkush <timofeyburkush@gmail.com>
2025-12-03 17:37:40 +0300

    - Загрузка контента из яндекс бакета

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿const CACHE_NAME = 'unity-game-cache-v38';
-const METADATA_CACHE = 'unity-game-metadata-v38';
+﻿const CACHE_NAME = 'unity-game-cache-v39';
+const METADATA_CACHE = 'unity-game-metadata-v39';
 const TTL_DAYS = 14;
 const TTL_MS = TTL_DAYS * 24 * 60 * 60 * 1000;
 
